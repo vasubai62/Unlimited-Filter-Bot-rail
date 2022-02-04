d878dedf566bb9a48db71d2af40969f4f9e78fd5
ZakirbhaiBott <87867737+ZakirbhaiBott@users.noreply.github.com>
2022-02-04 19:16:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -282,8 +282,12 @@ async def about(client, message):
         reply_to_message_id=message.message_id
     )
 
-photo=random.choice("https://telegra.ph/file/33acc0274d5b751d25414.mp4 https://telegra.ph/file/d8a14793481d469488ebe.mp4 https://telegra.ph/file/734656d1ee79c9ddda65b.mp4")
+GIF = [
+    "https://telegra.ph/file/33acc0274d5b751d25414.mp4"
+    "https://telegra.ph/file/d8a14793481d469488ebe.mp4"
+    "https://telegra.ph/file/734656d1ee79c9ddda65b.mp4"
+    ]
 
 @trojanz.on_message(filters.command('help') & filters.private)
 async def help(client, message):
-    await client.send_animation(chat_id=message.chat.id, animation=photo)
+    await client.send_animation(chat_id=message.chat.id, animation=random.choice(GIF))
