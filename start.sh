8a22a6b502fc46bde8818c052afb5530267b7081
ZakirbhaiBott <87867737+ZakirbhaiBott@users.noreply.github.com>
2022-02-04 15:37:46 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/ZakirbhaiBott/Unlimited-Filter-Bot-rail.git /Unlimited-Filter-Bot-rail
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Unlimited-Filter-Bot-rail
+fi
+cd /Unlimited-Filter-Bot-rail
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
