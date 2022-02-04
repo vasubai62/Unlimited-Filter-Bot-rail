750c3952a41679b5aa445f7c66d7ba0f514c607a
ZakirbhaiBott <87867737+ZakirbhaiBott@users.noreply.github.com>
2022-02-04 16:26:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -222,7 +222,7 @@ async def start(client, message):
                 ],
                 [
                     InlineKeyboardButton("Movies Group", url="https://t.me/malayayalies"),
-                    InlinekeyboardButton("Movies Channel", url="https://t.me/joinchat/Fhcssgw5H8wwMDBl")
+                    InlineKeyboardButton("Movies Channel", url="https://t.me/joinchat/Fhcssgw5H8wwMDBl")
                 ]
             ]
         ),
