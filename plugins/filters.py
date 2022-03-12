ccdfac179e5a2faf3e5fcc57e6f261fcdca21c76
ZakirbhaiBott <87867737+ZakirbhaiBott@users.noreply.github.com>
2022-03-12 23:50:05 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -341,14 +341,12 @@ async def give_filter(client,message):
                                 reply_text,
                                 disable_web_page_preview=True,
                                 reply_markup=InlineKeyboardMarkup(button)
-                                ttl_seconds(10)
                             )
                     else:
                         if btn == "[]":
                             await message.reply_cached_media(
                                 fileid,
                                 caption=reply_text or ""
-                                ttl_seconds(10)
                             )
                         else:
                             button = eval(btn) 
@@ -356,7 +354,6 @@ async def give_filter(client,message):
                                 fileid,
                                 caption=reply_text or "",
                                 reply_markup=InlineKeyboardMarkup(button)
-                                ttl_seconds(10)
                             )
                 except Exception as e:
                     print(e)
