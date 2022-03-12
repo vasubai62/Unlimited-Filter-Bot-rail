845645f731f7acbc0cc1b0b2e5543991ead656b3
ZakirbhaiBott <87867737+ZakirbhaiBott@users.noreply.github.com>
2022-03-13 00:07:17 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -350,12 +350,10 @@ async def give_filter(client,message):
                             )
                         else:
                             button = eval(btn) 
-                            k = await message.reply_cached_media(
+                            await message.reply_cached_media(
                                 fileid,
                                 caption=reply_text or "",
                                 reply_markup=InlineKeyboardMarkup(button)
-                                await asyncio.sleep(8)
-                                await k.delete()
                             )
                 except Exception as e:
                     print(e)
