301bcae061842411534aa8be5323a9d3e72fcf51
vasubai62 <98007108+vasubai62@users.noreply.github.com>
2022-08-10 17:54:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ZakirbhaiBott/Unlimited-Filter-Bot-rail.git /Unlimited-Filter-Bot-rail
+  git clone https://github.com/vasubai62/Unlimited-Filter-Bot-rail.git /Unlimited-Filter-Bot-rail
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Unlimited-Filter-Bot-rail
